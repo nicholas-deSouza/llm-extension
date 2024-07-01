f143800c1975683c045cb2e48e4a6292d7a4ab30
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-06-30 22:03:32 -0400

    message history finally works

--- llm-extension/backend/test.py
+++ llm-extension/backend/test.py
@@ -1,13 +1,25 @@
+# https://python.langchain.com/v0.2/docs/tutorials/chatbot/#quickstart
+
 import os
 from dotenv import load_dotenv
 import asyncio
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, trim_messages
+
+
 from fastapi.responses import StreamingResponse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 app = FastAPI()
 
 class Human_Message(BaseModel):
@@ -33,34 +45,54 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 model = ChatOpenAI(temperature=1, model_name="gpt-4o", api_key=OPENAI_API_KEY)
-prompt_not_cleaned = ChatPromptTemplate.from_template("tell me a joke about dragons. 400 words")
-prompt = prompt_not_cleaned.messages[0].prompt.template
-# prompt = "tell me a joke about cats. 200 words"
-parser = StrOutputParser()
 
 
-async def generate_chat_responses(input:str):
-   async for chunk in model.pipe(parser).astream(input):
-    #   content = chunk.replace("\n", "<br>")
-      yield f"{chunk}"
-      
+# message history and how it's managed
+store = {}
 
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+# trimmer = trim_messages(
+#     max_tokens=65,
+#     strategy="last",
+#     token_counter=model,
+#     include_system=True,
+#     allow_partial=False,
+#     start_on="human",
+# )
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+chain = (prompt | model)
+
+with_message_history = RunnableWithMessageHistory(chain, get_session_history)
+
+
+async def generate_chat_response(input):
+   
+   # session id seperates different sessions ie different convos
+   config = {"configurable": {"session_id": "abc15"}}
+   async for chunk in with_message_history.astream(
+        {
+            "messages": [HumanMessage(content=input)],
+        },
+        config=config,
+    ):
+    yield f"{chunk.content}"
 
 @app.post("/chat_stream/")
 async def chat_stream_events(text: Human_Message):
     input = text.userInput
-    return StreamingResponse(generate_chat_responses(input=input), media_type="text/markdown")
-
-
-
-# @app.get("/test")
-# async def test():
-
-#     chunks = []
-
-#     async for chunk in llm.astream("hello. tell me something about yourself"):
-#         chunks.append(chunk)
-#         print(chunk.content, end="|", flush=True)
-#         # return(chunk.content, end="|", flush=True)
-
-# # asyncio.run(test())
+    return StreamingResponse(generate_chat_response(input=input), media_type="text/markdown")
