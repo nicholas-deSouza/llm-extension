e40ce5da780cf9666be8830b8847fee567f5686d
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-07-01 21:10:12 -0400

    routing for chrome extensions must be done in main.tsx using hashrouter, must configure client id with supabase to enable it to work on chrome extensions (https://supabase.com/docs/guides/auth/social-login/auth-google?queryGroups=platform&platform=chrome-extensions)

--- llm-extension/frontend/src/App.tsx
+++ llm-extension/frontend/src/App.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // import { Home } from "./components/home";
 // import Settings from "./components/settingsPage/settings";
-import SignUpPage from "./components/signUpPage/signUpPage";
-import SuccessPage from "./components/successPage/successPage";
-import LoginPage from "./components/loginPage/loginPage";
-import HomePage from "./components/homePage/homePage";
-import ChatScreen from "./components/chatScreen/chatScreen";
+// import SignUpPage from "./components/signUpPage/signUpPage";
+// import SuccessPage from "./components/successPage/successPage";
+// import LoginPage from "./components/loginPage/loginPage";
+// import HomePage from "./components/homePage/homePage";
+// import ChatScreen from "./components/chatScreen/chatScreen";
+// import LoginPage from "./components/loginPage/loginPage";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          {/* redirects so that i don't manually have to do it */}
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<LoginPage />} />
+      {/* <Router>
+        <Routes> */}
+      {/* redirects so that i don't manually have to do it */}
+      {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+      {/* <Route path="#/" element={<ChatScreen />} /> */}
+      {/* <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/success" element={<SuccessPage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/chat" element={<ChatScreen />} />
-        </Routes>
-      </Router>
+          <Route path="/chat" element={<ChatScreen />} /> */}
+      {/* </Routes>
+      </Router> */}
     </>
   );
 }
--- llm-extension/frontend/src/main.tsx
+++ llm-extension/frontend/src/main.tsx
@@ -2,10 +2,65 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+// import LoginPage from "./components/loginPage/loginPage.tsx";
+import ChatScreen from "./components/chatScreen/chatScreen";
+// import { Home } from "./components/home";
+import Settings from "./components/settingsPage/settings";
+// import SignUpPage from "./components/signUpPage/signUpPage";
+import SuccessPage from "./components/successPage/successPage";
+import LoginPage from "./components/loginPage/loginPage";
+import HomePage from "./components/homePage/homePage";
+
+{
+  /* <Route path="/" element={<Navigate to="/login" />} /> */
+}
+{
+  /* <Route path="#/" element={<ChatScreen />} /> */
+}
+{
+  /* <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/success" element={<SuccessPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/chat" element={<ChatScreen />} /> */
+}
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  // {
+  //   path: "/",
+  //   element: <SignUpPage />,
+  // },
+  {
+    path: "/signup",
+    element: <Settings />,
+  },
+  {
+    path: "/success",
+    element: <SuccessPage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+  {
+    path: "/chat",
+    element: <ChatScreen />,
+  },
+  // {
+  //   path: "/app",
+  //   element: <App />,
+  // },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // the cause of multiple re renders
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
